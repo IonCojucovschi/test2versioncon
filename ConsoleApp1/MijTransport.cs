14c693f816667a0a2295161d5e46158db90a466b
IonCojucovschi <ion.cojucovschi@outlook.com>
2017-06-29 06:11:03 -0700

    CMT schema nr 2 relaise

---
+++ ConsoleApp1/MijTransport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class MijTransport
+    {
+        public String tip;
+        public String combustibil;
+        public String ruta;
+        public String numPasageri;
+        public String aniExploat;
+
+    }
+}
---
+++ ConsoleApp1/autobuze.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class autobuze : MijTransport
+    {
+        private String acid;
+        public String Conduc;
+        public int numRepar;
+    }
+}
