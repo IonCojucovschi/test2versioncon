a6f19f911c8cdd1fe3cd3acabe4060dc93d3e7d5
IonCojucovschi <ion.cojucovschi@outlook.com>
2017-06-29 06:26:39 -0700

    CMT2 adaogare muncitori

---
+++ ConsoleApp1/Lucratorii.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Lucratorii
+    {
+        public String nume;
+        public String preNume;
+        public int varsta;
+        public String functia;
+        public String rutaDEs;
+
+    }
+}
---
+++ ConsoleApp1/conducVeh.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class conducVeh
+    {
+        private String id_bul;
+        private float sarariu;
+        private String studii;
+        
+    }
+}
